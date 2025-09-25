74672c0d8090bfa3fd6ec60bed2e722d6f5ddc5f
honanr <reid.honan@unisa.edu.au>
2025-09-25 19:54:21 +0930

    Final commit for most recent changes

--- Thursday.py
+++ Thursday.py
@@ -1,2 +1,27 @@
+class student:
+    def __init__(self):
+        self.name = "henry"
+        self.age = 89
+
+    def grade(self):
+        return "I love my grade"
+
+
 print("hello, world!")
 print("Push next time")
+a = "Hi there"
+a = a.upper()
+print(a)
+
+stud = student()
+stud.grade()
+
+def add(a, b):
+    return a+b
+
+
+def subtraction(a, b):
+    return a-b
+
+
+
